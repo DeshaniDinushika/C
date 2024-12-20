39e046bcfdb5ec4377d938668e4962eb6b0ffe46
DeshaniDinushika <deshani23dinushika@gmail.com>
2024-12-20 21:09:45 +0530

    Add files via upload

---
+++ code1/index.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int myFunction(int a, int b)  {
+     return a+b;    
+}
+int main ()
+{
+       printf("Result is: %d", myFunction(15, 30));
+
+       printf("Result is: %d", myFunction(11345, 3890));
+
+return 0;
+}
+          
+
+        
+
+
+
+    return 0;
+
+ 
+ 
+
+
+       
+
+
+   
+
+
+
+
+
+
+
+   
